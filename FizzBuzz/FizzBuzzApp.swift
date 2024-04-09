3ad15e87c10ac296dd3861755c1e3fe2ff2f5e38
Bobby Chaudhry <bobbychaudhry@hotmail.co.uk>
2024-04-09 15:37:58 +0100

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Bobby Chaudhry on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
